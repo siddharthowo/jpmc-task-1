a8afb3dd025e810c85f772e6db73fbe9ae49c0bc
siddharth dey <91966026+siddharthowo@users.noreply.github.com>
2023-02-03 16:58:43 +0530

    Update client.py

--- client.py
+++ client.py
@@ -41,7 +41,7 @@ def getDataPoint(quote):
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    if(price_b==0)
+    if(price_b==0):
         return
     return price_a/price_b
 
